195c2569f96f77d65941fc4a53c8b210ea1ba5d4
Xiao Hu <xiaohu@vt.edu>
2023-04-09 14:03:57 -0400

    update Kavya's work

--- course_recommendation/views.py
+++ course_recommendation/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .forms import ResumeUploadForm
-from .models import Resume
-from .serializer import ResumeSerializer
+from .models import Resume, Courses
+from .serializer import ResumeSerializer, CourseSerializer
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from django.core import serializers
@@ -104,8 +104,8 @@ def prediction(skills):
         for skill in skills:
             X = transformer.transform([skill])
             y_pred = rec_model.predict(X)
-            pred.append(y_pred)
-        return pred
+            pred.append(y_pred[0])
+        return list(set(pred))
     except ValueError as e:
         return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
 
@@ -119,50 +119,10 @@ def ResumeFormView(request):
             resume = form.cleaned_data['resume_file']
             content = get_suggestions(resume)
             result = prediction(content)
-            return render(request, 'course_recommendation/prediction.html', {"data":result})
-        
+            courses = Courses.objects.filter(category = result[0]).values()[:5]
+            serialized_item = CourseSerializer(courses, many=True)
+            return render(request, 'prediction.html', {"data":serialized_item.data})
+    
     form = ResumeUploadForm()
     return render(request, 'course_recommendation/resume_upload.html', {"form":form})
 
-
-# def resume_upload(request):
-#     if request.method == 'POST':
-#         form = ResumeUploadForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             print("File uploaded successfully.")
-#             return render(request, 'success.html')
-#         else:
-#             print("Form is not valid.")
-#             print(form.errors)
-#     else:
-#         form = ResumeUploadForm()
-#     return render(request, 'resume_upload.html', {'form': form})
-
-
-# class StudentView(viewsets.ModelViewSet):
-#     queryset = Student.objects.all()
-#     serializer_class = StudentSerializer
-
-# def status(df):
-#     try:
-#         transformer = pickle.load(open("C:\\Users\\Kavya Venkatesh\\Capstone project\\OpenEdX_Course-main\\RecommendationSystem\\XiaoHu\\BoeingRecommendation\\course_recommendation\\transformer.sav", "rb"))
-#         rec_model = pickle.load(open("C:\\Users\\Kavya Venkatesh\\Capstone project\\OpenEdX_Course-main\\RecommendationSystem\\XiaoHu\\BoeingRecommendation\\course_recommendation\\rec_model.sav", "rb"))
-#         X = transformer.transform([df])
-#         y_pred = rec_model.predict(X)
-#         return y_pred
-#     except ValueError as e:
-#         return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
-    
-
-# def FormView(request):
-#     if request.method == "POST":
-#         form = StudentDetailsForm(request.POST or None)
-
-#         if form.is_valid():
-#             text = form.cleaned_data['text']
-#             result = status(text)
-#             return render(request, 'suggestion.html', {"data":result})
-        
-#     form = StudentDetailsForm()
-#     return render(request, 'form.html', {'form':form})
